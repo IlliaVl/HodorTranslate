3b22a86d93b7832533fc9a6fc971edea13d9ed1d
Vlasov Illia <vlasovia@gmail.com>
2017-11-15 15:01:23 +0200

    Initial commit

---
+++ HodorTranslate/HodorTranslate.h
@@ -0,0 +1,19 @@
+//
+//  HodorTranslate.h
+//  HodorTranslate
+//
+//  Created by Vlasov Illia on 11/15/17.
+//  Copyright © 2017 Illia Vlasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HodorTranslate.
+FOUNDATION_EXPORT double HodorTranslateVersionNumber;
+
+//! Project version string for HodorTranslate.
+FOUNDATION_EXPORT const unsigned char HodorTranslateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HodorTranslate/PublicHeader.h>
+
+
---
+++ HodorTranslate/HodorTranslate.swift
@@ -0,0 +1,20 @@
+//
+//  HodorTranslate.swift
+//  HodorTranslate
+//
+//  Created by Vlasov Illia on 11/15/17.
+//  Copyright © 2017 Illia Vlasov. All rights reserved.
+//
+
+import Foundation
+
+public class HodorTranslate {
+    
+    public static func translate(text: String) -> String {
+        return "Hodor!"
+    }
+    
+    public func translate(text: String) -> String {
+        return "Hodor!"
+    }
+}
